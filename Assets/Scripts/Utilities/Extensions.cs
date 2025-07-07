fe515335acda90cc973fafc48e5d1a56ab6dde4f
Blowerlop <Blowerlop@users.noreply.github.com>
2025-07-07 02:35:59 +0200

    Delete unused class

--- Assets/Scripts/Utilities/Extensions.cs
+++ Assets/Scripts/Utilities/Extensions.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace DeveloperConsole
+﻿namespace DeveloperConsole
 {
     namespace Extensions
     {
@@ -25,66 +22,7 @@ namespace DeveloperConsole
                 }
             }
         }
-        
-        public static class CollectionsExtensions
-        {
-            public static T Next<T>(this IReadOnlyCollection<T> list, ref int index)
-            {
-                return Next((IList<T>) list, ref index);
-            }
-            
-            public static T Previous<T>(this IReadOnlyCollection<T> list, ref int index)
-            {
-                if (list == null)
-                {
-                    throw new NullReferenceException();
-                }
-                if (list.Count == 0) return default;
-                
-                index--;
-                if (index < 0)
-                {
-                    index = list.Count - 1;
-                }
 
-                return Previous((IList<T>) list, ref index);
-            }
-            
-            public static T Next<T>(this IList<T> list, ref int index)
-            {
-                if (list == null)
-                {
-                    throw new NullReferenceException();
-                }
-                if (list.Count == 0) return default;
-                
-                index++;
-                if (index >= list.Count)
-                {
-                    index = 0;
-                }
-
-                return list[index];
-            }
-            
-            public static T Previous<T>(this IList<T> list, ref int index)
-            {
-                if (list == null)
-                {
-                    throw new NullReferenceException();
-                }
-                if (list.Count == 0) return default;
-                
-                index--;
-                if (index < 0)
-                {
-                    index = list.Count - 1;
-                }
-
-                return list[index];
-            }
-        }
-        
         public static class StringExtensions
         {
             public enum ESearchOrder
