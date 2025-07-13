2a3bf7973e4239e05de241247b1da67a0a71b613
Blowerlop <Blowerlop@users.noreply.github.com>
2025-07-14 01:49:10 +0200

    Fix console ConsoleParameterInputAttribute.cs

--- Assets/_Project/Runtime/Scripts/Console/Attributes/ConsoleParameterInputAttribute.cs
+++ Assets/_Project/Runtime/Scripts/Console/Attributes/ConsoleParameterInputAttribute.cs
@@ -22,35 +22,17 @@ namespace DeveloperConsole
 
             _func = memberInfo switch
             {
-                MethodInfo methodInfo when methodInfo.ReturnType != typeof(string) => throw new InvalidOperationException("The target method must return a string."),
+                MethodInfo methodInfo when methodInfo.ReturnType != typeof(string[]) => throw new InvalidOperationException("The target method must return a string."),
                 MethodInfo methodInfo => (Func<string[]>)Delegate.CreateDelegate(typeof(Func<string>), methodInfo),
                 
-                PropertyInfo propertyInfo when propertyInfo.PropertyType != typeof(string) => throw new InvalidOperationException("The target property must be a string."),
+                PropertyInfo propertyInfo when propertyInfo.PropertyType != typeof(string[]) => throw new InvalidOperationException("The target property must be a string."),
                 PropertyInfo propertyInfo => () => (string[])propertyInfo.GetValue(null),
                 
-                FieldInfo fieldInfo when fieldInfo.FieldType != typeof(string) => throw new InvalidOperationException("The target field must be a string."),
+                FieldInfo fieldInfo when fieldInfo.FieldType != typeof(string[]) => throw new InvalidOperationException("The target field must be a string."),
                 FieldInfo fieldInfo => () => (string[])fieldInfo.GetValue(null),
                 
                 _ => throw new ArgumentOutOfRangeException()
             };
-        }
-        
-        /// <param name="type">Enum type required</param>
-        public ConsoleParameterInputAttribute(Type type)
-        {
-            GenerateFuncByType(out _func);
-            return;
-
-            
-            // --- Local methods ---
-            void GenerateFuncByType(out Func<string[]> func)
-            {
-                if (type.IsEnum)
-                {
-                    func = () => Enum.GetNames(type);
-                }
-                else throw new InvalidOperationException("{type.Name} is not an enum.");
-            }
         }
 
         public string[] Resolve()
